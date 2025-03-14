86fe67c99831c0e69a023eb65ef01d08832c4938
Paul Christ <paul.l.christ@web.de>
2025-03-14 14:03:21 +0100

    feat: initial project setup with Express, TypeScript, ESLint and Prettier

---
+++ src/middlewares/middlewares.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.middleware = void 0;
+const middleware = (req, res) => {
+  res.send("Hello World!");
+  console.log("Response sent");
+};
+exports.middleware = middleware;
+//# sourceMappingURL=middlewares.js.map
